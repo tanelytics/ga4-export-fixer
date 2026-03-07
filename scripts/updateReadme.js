7e1080035ed02a22978770b1db47fc1e3e418664
Taneli Salonen <taneli.salonen@8-bit-sheep.com>
2026-03-07 13:42:13 +0200

    fix prepublish script

--- scripts/updateReadme.js
+++ scripts/updateReadme.js
@@ -3,6 +3,12 @@ Updates the version of the ga4-export-fixer package in the README.md file.
 */
 const fs = require('fs');
 const pkg = require('../package.json');
+
+if (pkg.version.includes('-')) {
+    console.log(`Skipping README update for dev version ${pkg.version}`);
+    process.exit(0);
+}
+
 let readme = fs.readFileSync('README.md', 'utf8');
 readme = readme.replace(/"ga4-export-fixer": "[\d.]+"/g, `"ga4-export-fixer": "${pkg.version}"`);
 fs.writeFileSync('README.md', readme);
